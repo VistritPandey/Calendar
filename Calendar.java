03f939ecd7e016a86566756d7b860480b3102b75
vistritpandey <vistritpandey@gmail.com>
2020-10-10 10:52:59 +0530

    Bugs Fixed

--- Calendar.java
+++ Calendar.java
@@ -21,9 +21,7 @@ import java.util.Scanner;
     }
     static void printMonthBody(int year, int month) {
       int startDay = getStartDay(year, month);
-      // Get number of days in the month
       int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
-      // Pad space before the first day of the month
       int i = 0;
       for (i = 0; i < startDay; i++)
         System.out.print("    ");
@@ -37,30 +35,23 @@ import java.util.Scanner;
       }
       System.out.println();
     }
-    /** Get the start day of the first day in a month */
    static int getStartDay(int year, int month) {
-      //Get total number of days since 1/1/1
       int startDay = 6;
       int totalNumberOfDays = getTotalNumberOfDays(year, month);
-      //Return the start day
       return (totalNumberOfDays + startDay) % 7;
     }
-    /** Get the total number of days since January 1, 1800 */
     static int getTotalNumberOfDays(int year, int month) {
      int total = 0;
-     //Get the total days from 1800 to year - 1
      for (int i = 1800; i < year; i++)
      if (isLeapYear(i))
 
         total = total + 366;
       else
         total = total + 365;
-      //Add days from January to the month prior to the calendar month
       for (int i = 1; i < month; i++)
         total = total + getNumberOfDaysInMonth(year, i);
       return total;
     }
-    /** Get the number of days in a month */
     static int getNumberOfDaysInMonth(int year, int month) {
       if (month == 1 || month == 3 || month == 5 || month == 7 ||
         month == 8 || month == 10 || month == 12)
@@ -69,9 +60,8 @@ import java.util.Scanner;
 
         return 30;
       if (month == 2) return isLeapYear(year) ? 29 : 28;
-      return 0; // If month is incorrect
+      return 0;
     }
-    /** Determine if it is a leap year */
     static boolean isLeapYear(int year) {
       return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
